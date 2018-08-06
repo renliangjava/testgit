5d983030462c42c757410aa2c27254c2dc075cbd
renliangjava <renliangjava@163.com>
2018-08-07 06:55:23 +0800

    first

---
+++ src/cn/tx/demo/Penguin.java
@@ -0,0 +1,31 @@
+package cn.tx.demo;
+
+class Penguin {
+	private String name = null; // 名字
+	private int health = 0; // 健康值
+	private String sex = null; // 性别
+	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	
+	
+
+	
+	
+	
+	
+}
---
+++ src/cn/tx/demo/Test.java
@@ -0,0 +1,12 @@
+package cn.tx.demo;
+
+class Parent {
+	
+}
+
+public class Test extends Parent { // 1
+	public Test(String Name) { // 2
+		
+		
+	}
+}
